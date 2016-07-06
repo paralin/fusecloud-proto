07aa8aedd780896688fcd6f783aba166140f2dc0
Christian Stewart <christian@paral.in>
2016-07-06 16:40:24 -0700

    Update auth pb

--- db/migrations/init/init.bash
+++ db/migrations/init/init.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-migrate -url cassandra://127.0.0.1:9042/global?protocol=4 -path ./ up
+CASSANDRA_IP=10.244.2.4
+migrate -url cassandra://$CASSANDRA_IP:9042/auth?protocol=4 -path ./ up
