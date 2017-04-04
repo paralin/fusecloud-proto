99b48485a29b93a5f3de99840bf0ae0533f45811
Christian Stewart <christian@paral.in>
2017-04-04 00:20:19 -0400

    refactor: remove old graphql type name code

--- region/graphql.types.go
+++
@@ -1,7 +0,0 @@
-package region
-
-// WARNING: This file is auto-generated.
-
-func (*Region) GraphQLTypeName() string {
-	return "Region"
-}
--- timesync/graphql.types.go
+++
@@ -1,7 +0,0 @@
-package timesync
-
-// WARNING: This file is auto-generated.
-
-func (*GetUnixTimeResponse) GraphQLTypeName() string {
-	return "GetUnixTimeResponse"
-}
